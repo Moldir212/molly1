e26d825ac5c100a112c92834f87a629de2fe846a
Moldir Tasbaltaeva <tasbaltaevamoldiir@outlook.com>
2026-02-20 20:48:23 +0500

    Add examples about the numbers

--- Practice 1/numbers.py
+++ Practice 1/numbers.py
@@ -1,3 +1,34 @@
 a = 3
 b = 4
 print((a*b)/2)
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+w=45
+z=3
+print(w%z)
+
+
+a = int(1.5)
+b = int(2.8)
+print(a)
+print(b)
